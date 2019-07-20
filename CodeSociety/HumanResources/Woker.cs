b11013267a9121d86bd63123380266fdc677b7d8
Simon <luiguangguan@live.com>
2019-07-20 18:27:57 +0800

    第一个版本

---
+++ CodeSociety/HumanResources/Woker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources
+{
+    /// <summary>
+    /// a woker objiect
+    /// </summary>
+    public class Woker
+    {
+        private Action _job;
+        private object _locker = new object();
+        private bool _starting = false;
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="job">worker's job</param>
+        public Woker(Action job)
+        {
+            _job = job;
+        }
+        #endregion
+
+        #region Start Working
+        /// <summary>
+        /// Start Working
+        /// start his job
+        /// </summary>
+        public void StartWorking()
+        {
+
+            lock (_locker)
+            {
+                if (!_starting)
+                {
+                    _locker = true;
+                    _job.Invoke();
+                    _locker = false;
+                }
+            }
+        }
+        #endregion
+
+    }
+}
